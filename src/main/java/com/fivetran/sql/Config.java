1b43805cf50329700e571fd938e3c79e0be1707a
Georgie <george@fivetran.com>
2014-09-16 22:41:35 -0700

    accept unknown properties

--- src/main/java/com/fivetran/sql/Config.java
+++ src/main/java/com/fivetran/sql/Config.java
@@ -1,5 +1,6 @@
 package com.fivetran.sql;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
@@ -15,6 +16,9 @@ public class Config {
 
         mapper.registerModule(new JSR310Module());
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        // TODO shouldn't be in stupid-sql
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return mapper;
     }
